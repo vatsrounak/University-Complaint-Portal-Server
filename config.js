15c3455e146f18fa1f5a4500a65a6630d9afb6e6
vatsrounak <rounakvats01@gmail.com>
2023-05-12 12:26:34 +0530

    config-update

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 export default {
-    JWT_SECRET : "patnanit",
-    ATLAS_URI: "mongodb+srv://nitpatna:nit123patna@nitpatna.ncyqvm9.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET : process.env.JWT_SECRET,
+    ATLAS_URI: process.env.ATLAS_URI,
     port: process.env.PORT || 8080,
-    // mongoURI: process.env.MONGO_URI || 'mongodb+srv://Abhi6722:Abhi6722@researchrover.ckwzho3.mongodb.net/?retryWrites=true&w=majority',
-    smtpHost: process.env.SMTP_HOST || 'smtp.hostinger.com',
-    smtpPort: process.env.SMTP_PORT || 465,
-    smtpUser: process.env.SMTP_USER || 'hackshark@heistat49.com',
-    smtpPassword: process.env.SMTP_PASSWORD || 'Hack@shark11',
+    smtpHost: process.env.SMTP_HOST,
+    smtpPort: process.env.SMTP_PORT ,
+    smtpUser: process.env.SMTP_USER ,
+    smtpPassword: process.env.SMTP_PASSWORD 
 }
